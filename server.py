874a4e77678a235b876ab2cbae1ee05369097620
Keigo Koizumi <al16043@shibaura-it.ac.jp>
2019-04-22 15:52:18 +0900

    修正

--- server.py
+++ server.py
@@ -1,3 +1,36 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+FROM_ADDRESS ='dustdetection707@gmail.com'
+MY_PASSWORD ='3993lily'
+TO_ADDRESS ='al16043@shibaura-it.ac.jp'
+BCC = 'receiver2@test.net'
+SUBJECT = 'Gmailでの数値送信'
+
+
+def create_message(from_addr, to_addr, bcc_addrs, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Bcc'] = bcc_addrs
+    msg['Date'] = formatdate()
+    return msg
+
+
+def send(from_addr, to_addrs, msg):
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.ehlo()
+    smtpobj.starttls()
+    smtpobj.ehlo()
+    smtpobj.login(FROM_ADDRESS, MY_PASSWORD)
+    smtpobj.sendmail(from_addr, to_addrs, msg.as_string())
+    smtpobj.close()
+
+
+
+
 from flask import Flask, request, render_template
 app = Flask(__name__)
 file_path = "./sensor_data.csv"
@@ -11,12 +44,17 @@ def update_dust():
     time = request.form["time"]
     dust = request.form["dust"]
     try:
-        f = open(file_path, 'w')
-        f.write(time + "," + dust)
-        return "succeeded to write"
+        to_addr = TO_ADDRESS
+        subject = SUBJECT
+        body =time+'・・・・・'+dust
+
+        msg = create_message(FROM_ADDRESS, to_addr, BCC, subject, body)
+        send(FROM_ADDRESS, to_addr, msg)
+        print "success"
+        return "succeeded to send email"
     except Exception as e:
         print(e)
-        return "failed to write"
+        return "failed to send email"
     finally:
         f.close()
 @app.route('/dust', methods=['GET'])
